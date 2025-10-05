4d7a3a08e38dbeffb3dfb84ed6d3db888a3137f7
luisa-dla <luisaalves.bio@gmail.com>
2025-10-05 14:39:33 -0300

    Update legendas.py

--- legendas.py
+++ legendas.py
@@ -1,10 +1,16 @@
-def main():
-    original = input("Legenda original: ")
-    print(legendas(original))
+import re
+import sys
 
+def main():
+    with open(sys.argv[1], "r") as f:
+        original = f.read()
+    legendas(original)
+    
 def legendas(original):
-    corrigida = original.replace("->", "-->").replace(": ", ":")
-    return corrigida
+  corrigida = original.replace("->", "-->").replace(": ", ":")
+    with open(sys.argv[1], "w") as f:
+        f.write(corrigida)
 
 if __name__ == "__main__":
     main()
+    
